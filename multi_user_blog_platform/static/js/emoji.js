c5355bb973734d639d3ca6f824c38fee0ed5d93d
ChesterLen <svetoslavbaykov55@gmail.com>
2025-07-30 17:58:43 +0300

    Emoji setup

---
+++ multi_user_blog_platform/static/js/emoji.js
@@ -0,0 +1,10 @@
+// const EMOJI_API_KEY = 'e257da07aedad0c6c7573d0c6fad8727ae2b2a71';
+// const EMOJI_URL = `https://emoji-api.com/emojis?access_key=${EMOJI_API_KEY}`;
+
+// fetch(EMOJI_URL)
+//     .then(res => res.json())
+//     .then(body => {
+//         for (const emoji of body) {
+//             console.log(emoji);
+//         };
+//     });
